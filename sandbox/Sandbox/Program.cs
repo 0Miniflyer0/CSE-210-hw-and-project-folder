8f012bbbf633f4afa3109ba98af31a753090aebf
0MiniFlyer0 <guyaaron21@gmail.com>
2024-09-25 13:03:59 -0600

    Update

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
+        List<string> x;
+        x = ["twenty four", "twenty five", "twenty six"] ;
+
         Console.WriteLine("Hello Sandbox World!");
+
+        foreach (string number in x)
+        {
+            Console.WriteLine(number);
+        }
+        
     }
 }
