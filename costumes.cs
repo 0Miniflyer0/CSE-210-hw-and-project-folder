2ebbeab05cfcae6979e4db10f66cdd4e8e367b6e
0MiniFlyer0 <guyaaron21@gmail.com>
2024-10-03 10:29:28 -0600

    Wk3 updated

---
+++ costumes.cs
@@ -0,0 +1,20 @@
+class Costume
+{
+    string headwear;
+
+    string gloves;
+
+    string shoes;
+
+    string uppergarment;
+
+    string lowergarment;
+
+    string accessory;
+
+    void Showwardrobe()
+    (
+        string result = "";
+
+    )
+}
